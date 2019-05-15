107a27336008bcf52e2e195e5ba098787524125f
Okafor Jude <Okaforjudechukwuebuka@gmail.com>
2019-05-15 20:48:48 +0100

    set up travis and babel

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to jude app');
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
