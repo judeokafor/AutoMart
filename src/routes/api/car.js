617634011fdf435f78e6c81b77bf509f36b64152
Okafor Jude <Okaforjudechukwuebuka@gmail.com>
2019-05-21 07:00:34 +0100

    update for travis

--- src/routes/api/car.js
+++ src/routes/api/car.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import passport from 'passport';
-import upload from '../lib/config/multerConfig';
-import carController from './cars';
-import isAdmin from '../lib/middleware/admin';
+import upload from '../../lib/config/multerConfig';
+import carController from '../../controllers/cars';
+import isAdmin from '../../lib/middleware/admin';
 
 const router = express.Router();
 
