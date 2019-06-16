08e37732f955ea47d27545d0ee55267ade7c90b1
Okafor Jude <Okaforjudechukwuebuka@gmail.com>
2019-06-16 21:52:54 +0100

    switch db config to local

--- src/lib/config/dbConfig.js
+++ src/lib/config/dbConfig.js
@@ -9,8 +9,8 @@ if (process.env.NODE_ENV === 'test') {
   connectionString = process.env.PGURLTEST;
   ssl = false;
 } else {
-  connectionString = process.env.PGHEROKU;
-  ssl = true;
+  connectionString = process.env.PGURLDEV;
+  ssl = false;
 }
 
 const pool = new Pool({
