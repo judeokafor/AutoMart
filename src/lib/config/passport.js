2b9f7a6a28851f4c1ea2f43e920a153b755ef774
Okafor Jude <Okaforjudechukwuebuka@gmail.com>
2019-06-16 21:35:15 +0100

    fix:(passport) fix options

--- src/lib/config/passport.js
+++ src/lib/config/passport.js
@@ -2,23 +2,28 @@ import JwtStrategy from 'passport-jwt';
 import dotenv from 'dotenv';
 import Queries from '../helpers/queries';
 import db from '../helpers/dbHelpers';
+import ErrorHandler from '../helpers/errorHandler';
 
 const { Strategy, ExtractJwt } = JwtStrategy;
 dotenv.config();
 
-const opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = process.env.SECRET_KEY;
 const passportFunction = (passport) => {
+  const opts = {};
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+  opts.secretOrKey = process.env.SECRET_KEY;
   passport.use(
     new Strategy(opts, async (jwtPayLoad, next) => {
-      const args = [jwtPayLoad.email];
-      const { rows } = await db.Query(Queries.searchForEmail, args);
-      const user = rows[0];
-      if (user) {
-        next(null, user);
-      } else {
-        next(null, false);
+      try {
+        const args = [jwtPayLoad.email];
+        const { rows } = await db.Query(Queries.searchForEmail, args);
+        const user = rows[0];
+        if (user) {
+          next(null, user);
+        } else {
+          next(null, false);
+        }
+      } catch (error) {
+        console.log(error);
       }
     }),
   );
