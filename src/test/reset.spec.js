015e7da28fd1a8ed74f7ce66fc01735a97cce4bd
Okafor Jude <Okaforjudechukwuebuka@gmail.com>
2019-06-18 09:00:19 +0100

    fix: forbiden error

--- src/test/reset.spec.js
+++ src/test/reset.spec.js
@@ -20,12 +20,6 @@ describe('Testing the reset password functionality', () => {
         .send(testData.signInUser());
       const { token } = res.body;
       auth = token;
-      const resp = await chai
-        .request(server)
-        .post(`${base2}/signIn`)
-        .send(testData.signInAdmin());
-      const rez = resp.body;
-      adminauth = rez.token;
     } catch (error) {
       console.log(error);
     }
@@ -41,15 +35,10 @@ describe('Testing the reset password functionality', () => {
       const res = await chai
         .request(server)
         .post(`${base}/okaforjudechukwuebuka@gmail.com/reset_password`)
+        .type('form')
+        .send({ newPassword: '1234abcddA2' })
         .set('Authorization', auth);
       expect(res).to.have.status(204);
-    });
-    it('should return a forbidden error', async () => {
-      const res = await chai
-        .request(server)
-        .post(`${base}/okaforjudechukwuebuka@gmail.com/reset_password`)
-        .set('Authorization', adminauth);
-      expect(res).to.have.status(403);
     });
     it('should return an error if unauthorized user', async () => {
       const res = await chai
@@ -76,5 +65,28 @@ describe('Testing the reset password functionality', () => {
       expect(res.body).to.have.property('status');
       expect(res.body).to.have.property('message');
     });
+  });
+});
+describe('Testing the forbidden route', () => {
+  before(async () => {
+    try {
+      const res = await chai
+        .request(server)
+        .post(`${base2}/signIn`)
+        .send(testData.signInAdmin());
+      const { token } = res.body;
+      adminauth = token;
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  it('should return a forbidden error', async () => {
+    const res = await chai
+      .request(server)
+      .post(`${base}/okaforjudechukwuebuka@gmail.com/reset_password`)
+      .type('form')
+      .send({ newPassword: '1234abcddA2', password: 'jude11aaww' })
+      .set('Authorization', adminauth);
+    expect(res).to.have.status(403);
   });
 });
